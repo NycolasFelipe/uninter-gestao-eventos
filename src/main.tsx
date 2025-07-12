41bc3744600f69f2b6dc31f484bb5114f967140d
Nycolas Felipe <nycolasfelipe.contato@gmail.com>
2025-07-12 13:11:51 -0300

    feat(main): Inicialização do react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App.tsx'
 
+// React Query
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </QueryClientProvider>
 )
